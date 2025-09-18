c966f6b650dfad526f35e17c5cd344be8deb5005
Nicholas_UFC <nicholas.fm4@gmail.com>
2025-09-18 16:46:57 -0300

    Foi configurado allowed_hosts para permitidos *

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-(derufej*ffe+m$sfny3c+%&67tt^d)4)lo0styh+83&%ipq7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
